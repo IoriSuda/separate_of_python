2c68f81c1ac080ce9c8f15df50c6a53b82988491
IoriSuda <118954866+IoriSuda@users.noreply.github.com>
2025-06-26 21:12:58 +0900

    a

--- output_YNAN/test.py
+++ output_YNAN/test.py
@@ -1,12 +1,7 @@
-import unicodedata
-
-def normalize(s):
-    return unicodedata.normalize('NFKC', s.strip())
-
-
-
-
 #表記ゆれ
+#mapping_dictのループは別で回す? -> 意味ないみたい
+
+
 mapping_dict = {
     "BOLIVIA" : "BOLIVIA (PLURINATIONAL STATE OF)",
     "CAPE VERDE" : "CABO VERDE",
@@ -20,6 +15,11 @@ mapping_dict = {
     "VENEZUELA" : "VENEZUELA (BOLIVARIAN REPUBLIC OF)"
 }
 
+
+
+
+
+
 #かつて存在したlist_bにのみ存在する国リスト
 only_b_list = [
     "YUGOSLAVIA"
@@ -32,55 +32,59 @@ file_b = open('b.txt', 'r', encoding='UTF-8')
 a_list = [line.strip() for line in file_a.readlines()]
 b_list = [line.strip() for line in file_b.readlines()]
 
-print(len(b_list))
-#2-1から条件分岐かけていく？
-
-count_a = count_b = roop_counter = 0
 
 
 
-# ...
+#print(len(b_list)) -> 少ない方の国名リスト
 
-while roop_counter < len(b_list):
-    print(roop_counter)
-    a_item = normalize(a_list[roop_counter])
-    b_item = normalize(b_list[roop_counter])
-    print(repr(a_item))
-    print(repr(b_item))
+#表記ゆれの確認
 
-    for dict_key in mapping_dict.keys():
-        key = normalize(dict_key)
-        print(repr(key))
-        if a_item == key or b_item == key:
-            print("一致！")
-            break
-
-    roop_counter += 1
-
-
-'''
-
-listは0から始まるので、whileで回すときは、(行数-1)回ループするようにする
-
-#表記ゆれの国かどうか一行ずつ確認する。表記ゆれでない場合、次の処理に進む。
-while roop_counter < len(b_list):
+def spelling_discrepancies():
     
-    for map_keys in mapping_dict.keys():
-        #表記ゆれの国なら...
-        if (a_list[count_a] == map_keys) or (b_list[count_b] == map_keys):
-            print(a_list[count_a])
-            print(b_list[count_b])
-            print(map_keys)
-            count_a += 1
-            count_b += 1
-            roop_counter += 1
+    for i in range(len(b_list)):
+        
+        for key_dict in mapping_dict.keys():
             
-        else:
-            count_a += 1
-            count_b += 1
-            roop_counter += 1
+            if (key_dict == b_list[i]):
+                print(i)
+                get_b = b_list[i]
+                print("FIND DIFFERENT key_dict:" + str(mapping_dict[key_dict]) +  "  b_list:" + str(get_b))
+            else:
+                print("DON'T FIND DIFFERENT  key_dict:"  + str(key_dict) + "  b_list:" + str(b_list[i]))    
+    
+only_a_list = []
+count_a = count_b = 0
+for i in range(len(b_list)):
+    
+    if a_list[count_a] != b_list[count_b]:
+        print("FIND DIFFERENT")
+        print("a_list:" + str(a_list[count_a]) + "  b_list:" + str(b_list[count_b]))
+        only_a_list.append(a_list[count_a])
+        substract = i
+        for l in range(len(a_list) - substract):
+            if a_list[l] == b_list[count_b]:
+                count_a = l
+                print("count_a: " + str(count_a))
+                print("count_b: " + str(count_b))
+                continue
+            else:
+                only_b_list.append(b_list[count_b])     
+                count_a += 1
+                print("count_a: " + str(count_a))
+                print("count_b: " + str(count_b))
+        
+        
+        
+        
+    else:
+        print("DON'T FIND DIFFERENT  a_list:" + str(a_list[i]) + "  b_list:" + str(b_list[i]))
+        count_a += 1
+        count_b += 1
 
 
-'''
+for i in range(len(only_a_list)):
+    print("only_b_list  " + str(i) + ":" + str(only_a_list[i]))
 
+for i in range(len(only_b_list)):
+    print("only_b_list  " + str(i) + ":" + str(only_b_list[i]))
 
